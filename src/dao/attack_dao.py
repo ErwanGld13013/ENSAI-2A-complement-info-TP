61f4cca1bc6603349937ab98b4ce0982ff86a9bb
erwangld13 <erwan.gilland@eleve.ensai.fr>
2025-09-19 10:43:02 +0000

    tp4 4.1

--- src/dao/attack_dao.py
+++ src/dao/attack_dao.py
@@ -42,6 +42,65 @@ class AttackDao(metaclass=Singleton):
             created = True
 
         return created
+    
+    
+    def find_attack_by_id(self, id_attaque: int) -> AbstractAttack:
+        """
+        Retourne l'attaque correspondante à son id
+        """
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """SELECT * FROM tp.attack a2
+                    INNER JOIN tp.attack_type at2 USING (id_attack_type)
+                    WHERE a2.id_attack = %(id_attaque)s ;""",
+                    {"id_attaque": id_attaque}
+                )
+                #Retourner le resultat
+                result = cursor.fetchone()
+            
+            attack = None
+            if result:
+                attack = AttackFactory().instantiate_attack(
+                    type = result["attack_type_name"],
+                    id = result["id_attack"],
+                    power = result["power"],
+                    name = result["attack_name"],
+                    description = result["attack_description"],
+                    accuracy = result["accuracy"],
+                    element = result["element"]
+                )
+            return attack
+
+    def find_all_attack(self) -> List[AbstractAttack]:
+        """
+        Retourne l'attaque correspondante à son id
+        """
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """SELECT * FROM tp.attack a2
+                    INNER JOIN tp.attack_type at2 USING (id_attack_type);"""
+                )
+                #Retourner le resultat
+                result = cursor.fetchall()
+            
+            all_attack = []
+            if result:
+                for attaq in result:
+
+                    val = AttackFactory().instantiate_attack(
+                        type = attaq["attack_type_name"],
+                        id = attaq["id_attack"],
+                        power = attaq["power"],
+                        name = attaq["attack_name"],
+                        description = attaq["attack_description"],
+                        accuracy = attaq["accuracy"],
+                        element = attaq["element"]
+                    )
+                    all_attack.append(val)
+
+            return all_attack
 
 
 if __name__ == "__main__":
@@ -60,5 +119,12 @@ if __name__ == "__main__":
         element="Normal",
     )
 
-    succes = AttackDao().add_attack(mon_attaque)
-    print("Attack created in database : " + str(succes))
+    #succes = AttackDao().add_attack(mon_attaque)
+    #print("Attack created in database : " + str(succes))
+    dao = AttackDao()
+    test = dao.find_attack_by_id(3)
+    print(test.power)
+    test2 = dao.find_all_attack()
+    print(test2[0].power)
+
+
